51160db3ee0832da09dd460c1e1565c9d0521824
igorg-rc <igor.gayvoronsky-rc@gmail.com>
2020-11-09 16:12:12 +0200

    Add credentials

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
   res.send({ hello: 'there' });
 });
 
-const PORT = process.env.PORT || 5000;
 
 app.listen(PORT);
