716d59f54446ca1461f5fda85ca7c67de7f55f0b
igorg-rc <igor.gayvoronsky-rc@gmail.com>
2020-11-11 01:49:51 +0200

    changed

--- config/dev.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  googleClientID: '297742832811-0gj4kvphqf50tdh06tuq1jpb7sk1o710.apps.googleusercontent.com',
-  googleClientSecret: 'KH25liMnitkFt8GiWPeIw-if',
-  mongoURI: 'mongodb+srv://igor-1987:7KN2Cwxcc0Rtj7Y4@cluster0.mgaax.mongodb.net/emaily-dev?retryWrites=true&w=majority',
-  cookieKey: 'asjdkhsajkdhsuydsyd3434mn3m433'
-};
