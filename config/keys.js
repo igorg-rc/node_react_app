fd2ef4d4e22baad93b1dd520b8fe80e763ab3a0e
igorg-rc <igor.gayvoronsky-rc@gmail.com>
2020-11-10 22:28:24 +0200

    changes in config folder

--- config/keys.js
+++
@@ -1,8 +0,0 @@
-// keys.js
-if (process.env.NODE_ENV === 'production') {
-  // we are in production - return the prod set of keys
-  module.exports = require('./prod')
-} else {
-  // we are in development - return dev set of keys
-  module.exports = require('./dev');
-}
