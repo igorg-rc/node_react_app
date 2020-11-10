cc9c32de54ea751564cfc41fe60054da2fe98ed0
igorg-rc <igor.gayvoronsky-rc@gmail.com>
2020-11-10 22:48:10 +0200

    config

--- config/dev.js
+++ config/dev.js
@@ -1,6 +1,7 @@
 module.exports = {
   googleClientID: '297742832811-0gj4kvphqf50tdh06tuq1jpb7sk1o710.apps.googleusercontent.com',
   googleClientSecret: 'AIzaSyBt1UCGXgvYRrlgnGev0wjNVuji3SHsEOg',
+  googleClientCallback: '/auth/google/callback',
   mongoURI: 'mongodb+srv://igor-1987:7KN2Cwxcc0Rtj7Y4@cluster0.mgaax.mongodb.net/emaily-dev?retryWrites=true&w=majority',
-  cookieKey: 'asjdkhsajkdhsuydsydwwidwwdwdwds'
+  cookieKey: 'asjdkhsajkdhsuydsydwSKADKSLD1213'
 };
